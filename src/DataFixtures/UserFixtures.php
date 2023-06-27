9acd82509416153585fdb5b8c551115860a90ebd
chado <a.sale@outlook.fr>
2023-06-27 14:32:08 +0200

    fix: make more user with faker and add haser password

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,15 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
+
         $user = new User();
         $user->setEmail('a.sale@outlook.fr');
-        $user->setPassword('fw7jzpdr7');
+        $user->setPassword($this->hasher->hashPassword($user, 'fw7jzpdr7'));
         $user->setFirstname('Alexandre');
         $user->setLastname('Sale');
         $user->setPhone('0606060606');
@@ -24,7 +35,7 @@ class UserFixtures extends Fixture
 
         $user = new User();
         $user->setEmail('chadowow@outlook.fr');
-        $user->setPassword('fw7jzpdr7');
+        $user->setPassword($this->hasher->hashPassword($user, 'fw7jzpdr7'));
         $user->setFirstname('Alexandre');
         $user->setLastname('Sale');
         $user->setPhone('0606060606');
@@ -32,6 +43,18 @@ class UserFixtures extends Fixture
         $user->setIsVerified(true);
         $manager->persist($user);
 
+
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+            $user->setFirstname($faker->firstName());
+            $user->setLastname($faker->lastName());
+            $user->setEmail('user' . $i . '@jobitbetter.com');
+            $user->setPassword($this->hasher->hashPassword($user, 'user'));
+            $user->setRoles(['ROLE_USER']);
+            $user->setPhone('0606060606');
+            $user->setIsVerified(true);
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
