7a2c3a061ad3b3661255f0037ee000cde4d1cc36
chado <a.sale@outlook.fr>
2023-07-01 00:22:46 +0200

    Feat: Install Tailwind

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,19 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import 'tw-elements'
+// Initialization for ES Users
+import {
+    Collapse,
+    Dropdown,
+    initTE,
+    Ripple,
+    Input
+
+} from "tw-elements";
+
+initTE({ Collapse, Dropdown, Ripple, Input });
+
+
