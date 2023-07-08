d7f71f2b2581f652829962f5e8cd74d0a662f270
chado <a.sale@outlook.fr>
2023-07-08 18:58:44 +0200

    Feat: it's not realy a big pr :p

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 import 'tw-elements'
+import './switch'
 // Initialization for ES Users
 import {
     Modal,
