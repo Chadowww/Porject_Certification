96b3d4f79bc8c5b556d9b0776efab690b1b1da55
chado <a.sale@outlook.fr>
2023-07-12 08:45:16 +0200

    Feat: update feild display in form edit

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +16,16 @@ class UserType extends AbstractType
         $builder
             ->add('email')
 //            ->add('roles')
-            ->add('password')
+//            ->add('password')
             ->add('firstname')
             ->add('lastname')
             ->add('phone')
+            ->add('adress', TextType::class, [
+                'required' => false,
+            ])
+            ->add('city')
+            ->add('code')
+            ->add('country')
 //            ->add('isVerified')
 //            ->add('createdAt')
 //            ->add('updatedAt')
