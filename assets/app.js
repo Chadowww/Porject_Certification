20f602e3250b757405403fe005a441ce7fdd980c
chado <a.sale@outlook.fr>
2023-07-15 12:40:37 +0200

    Feat: add function for favorite book async

--- assets/app.js
+++ assets/app.js
@@ -23,4 +23,31 @@ import {
 
 initTE({ Collapse, Dropdown, Ripple, Input, Modal });
 
+const test = document.querySelectorAll('.bt-fav');
 
+function addToFavorite(event) {
+    event.preventDefault();
+
+    const favLink = event.currentTarget;
+    const link = favLink.href;
+
+    try{
+        fetch(link)
+            .then(response => response.json())
+            .then(data => {
+                const favIcon = favLink.firstElementChild;
+                if(data.isFavorite){
+                    favIcon.classList.remove('bi-heart');
+                    favIcon.classList.add('bi-heart-fill');
+                }else {
+                    favIcon.classList.remove('bi-heart-fill');
+                    favIcon.classList.add('bi-heart');
+                }
+            });
+    }catch(e){
+        console.log(e);
+    }
+
+}
+window.addToFavorite = addToFavorite;
+// test.addEventListener('click',() => {console.log('connard')});
