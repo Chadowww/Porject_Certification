190e3ab318775afc1143b90bb40be80c71a9834e
chado <a.sale@outlook.fr>
2023-07-20 01:52:33 +0200

    Fix: update variable env for mailer admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address($_ENV['MAIL_ADMIN'], 'Alexandre Salé'))
+                    ->from(new Address($_ENV['MAILER_ADMIN'], 'Alexandre Salé'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
