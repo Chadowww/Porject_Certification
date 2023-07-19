e56bc1dfb05a3607601ea03371c7f341d532ce35
chado <a.sale@outlook.fr>
2023-07-20 01:49:59 +0200

    Feat: add 'controller' for secure form

--- assets/Scripts/validator.js
+++ assets/Scripts/validator.js
@@ -1,16 +1,58 @@
-import Validator from './js-form-validator';
+// import Validator from './js-form-validator.js';
+// const Validator = require('./js-form-validator.min.js');
+import { Validator } from './js-form-validator.min.js';
 
+
+//////////////
+console.log(Validator)
 const firstname = document.querySelector('#user_firstname');
 const lastname = document.querySelector('#user_lastname');
 const email = document.querySelector('#user_email');
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#confirm-password');
 
+
+
 // Get form handle
- let formHandle = document.querySelector('.form-user-edit');
 
 if(document.querySelector('.form-user-edit')){
- new Validator(document.querySelector('.form-user-edit'), function (err, res) {
-  return res;
- });
+    let formHandle = document.querySelector('.form-user-edit');
+
+    new Validator(formHandle, function (err, res) {
+
+        // some code of success of validation
+        return res;
+    }, {
+        // set auto tracking forcibly
+        autoTracking: true
+    });
+// Got to validation
 }
+
+if (document.querySelector('.form-user-login')){
+    let formHandleLogin = document.querySelector('.form-user-login');
+
+    new Validator(formHandleLogin, function (err, res) {
+
+        // some code of success of validation
+        return res;
+    }, {
+        // set auto tracking forcibly
+        autoTracking: true
+    });
+// Got to validation
+}
+if (document.querySelector('.form-user-register')) {
+    console.log('register')
+    let formHandleRegister = document.querySelector('.form-user-register');
+
+    new Validator(formHandleRegister, function (err, res) {
+
+        // some code of success of validation
+        return res;
+    }, {
+        // set auto tracking forcibly
+        autoTracking: true
+    });
+}
+
