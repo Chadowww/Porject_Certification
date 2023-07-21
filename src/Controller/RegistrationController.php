918e9c417dc1a181eed6760e4e6e6aaadeaedcee
chado <a.sale@outlook.fr>
2023-07-21 10:38:07 +0200

    Fix: translate flash message

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -89,7 +89,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre compte a bien été vérifié .');
 
         return $this->redirectToRoute('app_home');
     }
