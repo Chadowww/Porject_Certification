dc61b61a24ab1233dfd5fa8dc41268cd2b2c2ee7
chado <a.sale@outlook.fr>
2023-07-23 00:50:58 +0200

    Feat: Update form BookType

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,7 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\Category;
+use App\Entity\Editor;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -22,6 +28,37 @@ class BookType extends AbstractType
             ])
             ->add('qteStock')
             ->add('qteCheckout')
+            ->add('cover')
+            ->add('author',EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('a')
+                        ->orderBy('a.name', 'ASC');
+                },
+            ] )
+            ->add('category',EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+            ] )
+            ->add('editor',EntityType::class, [
+                'class' => Editor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('e')
+                        ->orderBy('e.name', 'ASC');
+                },
+            ])
         ;
     }
 
