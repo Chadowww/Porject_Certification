082ce59cdf977e6bc5229d52cba5b5e150b15948
chado <a.sale@outlook.fr>
2023-07-23 20:08:33 +0200

    Feat: Update form new user for admin

--- src/Form/AdminUserType.php
+++ src/Form/AdminUserType.php
@@ -2,27 +2,79 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdminUserType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('phone')
-            ->add('isVerified')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('adress')
-            ->add('code')
-            ->add('city')
-            ->add('country')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'required' => true,
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    // Ajoutez d'autres rôles ici si nécessaire
+                ],
+                'multiple' => true, // Permet de sélectionner plusieurs rôles
+                'expanded' => true, // Affiche les rôles en tant que cases à cocher plutôt qu'un menu déroulant
+                'required' => false,
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => true,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+            ])
+            ->add('isVerified', CheckboxType::class, [
+                'label' => 'Vérifié',
+                'required' => false,
+            ])
+            ->add('adress', TextType::class, [
+                'label' => 'Adresse',
+                'required' => false,
+            ])
+            ->add('code', IntegerType::class, [
+                'label' => 'Code postal',
+                'required' => false,
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'required' => false,
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays',
+                'required' => false,
+            ])
         ;
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
     }
 }
