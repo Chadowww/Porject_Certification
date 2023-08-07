2e1a6a9f40b2257a3c8554df8c83e5c11a8c9d36
chado <a.sale@outlook.fr>
2023-08-07 07:22:00 +0200

    Feat Created new extension Rand for doctrine

--- src/Extensions/Doctrine/Rand.php
+++ src/Extensions/Doctrine/Rand.php
@@ -2,7 +2,24 @@
 
 namespace App\Extensions\Doctrine;
 
-class Rand
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\QueryException;
+
+class Rand extends \Doctrine\ORM\Query\AST\Functions\FunctionNode
 {
 
+    /**
+     * @throws QueryException
+     */
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'RAND()';
+    }
 }
